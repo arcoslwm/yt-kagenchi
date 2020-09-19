a3486188bb2ea33135fed5624d091a66228d9e94
Arcos.lwm <arcos.lwm@gmail.com>
2020-09-18 23:06:32 -0300

    agrega decodificacion de url

--- background.js
+++ background.js
@@ -7,41 +7,44 @@ browser.contextMenus.create({
 browser.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === "yt-kagenchi") {
 
+
         console.log('yt-kagenchi info.linkUrl: ', info.linkUrl);
-        let linkUrl = info.linkUrl;
+        let linkUrl = (info.linkUrl.indexOf("%") !== -1) ?
+                        decodeURIComponent(info.linkUrl) : info.linkUrl;
+
         let videoId = '';
 
-        if( linkUrl.indexOf("youtube") !== -1 &&
+        if (linkUrl.indexOf("youtube") !== -1 &&
             linkUrl.indexOf("v=") !== -1 &&
-            linkUrl.substr(linkUrl.indexOf("v=")+2).length >= 11 )
+            linkUrl.substr(linkUrl.indexOf("v=") + 2).length >= 11)
         {
-    		videoId = linkUrl.substr( linkUrl.indexOf("v=")+2 , 11 );
-    	}
-    	else if( linkUrl.indexOf("youtu.be/") !== -1 &&
-                 linkUrl.substr( linkUrl.indexOf(".be/")+4 ).length >= 11 )
+            videoId = linkUrl.substr(linkUrl.indexOf("v=") + 2, 11);
+        }
+        else if (linkUrl.indexOf("youtu.be/") !== -1 &&
+                 linkUrl.substr(linkUrl.indexOf(".be/") + 4).length >= 11)
         {
-            videoId = linkUrl.substr(linkUrl.indexOf(".be/")+4,11);
-    	}
+            videoId = linkUrl.substr(linkUrl.indexOf(".be/") + 4, 11);
+        }
         else {
             console.log("video id not found");
-    	}
+        }
 
-        if(videoId!==''){
-            console.log('open video: ',videoId);
+        if (videoId !== '') {
+            console.log('open video: ', videoId);
             let creating = browser.tabs.create({
-                active : false,
-                url: "https://www.youtube.com/embed/"+videoId
+                active: false,
+                url: "https://www.youtube.com/embed/" + videoId
             });
             creating.then(onCreated, onError);
-		}
+        }
 
     }
 });
 
 function onCreated(tab) {
-  console.log(`Created new tab: ${tab.id}`)
+    console.log(`Created new tab: ${tab.id}`)
 }
 
 function onError(error) {
-  console.log(`Error: ${error}`);
+    console.log(`Error: ${error}`);
 }
